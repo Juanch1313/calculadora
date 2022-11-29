b725ca7f5ec2aaf635a35d35cd40c052b8ef3df3
Jhonny <juanchitorodi1312@gmail.com>
2022-11-28 18:55:22 -0600

    Calculator project

---
+++ src/componentes/Logotipo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import freeCodeCampLogo from '../imagenes/logo-freecodecamp.png';
+
+const Logotipo = () => (
+  <img 
+        className='freecodecamp-logo'
+        src={freeCodeCampLogo}
+        alt='Logo de freeCodeCamp' />
+);
+
+export default Logotipo;
